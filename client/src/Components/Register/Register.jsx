42f107b36e93138dfd72d3464c046c85bbe35f80
Chadwick Green <chadggreen@gmail.com>
2021-08-26 10:30:34 -0400

    registration page form

--- client/src/Components/Register/Register.jsx
+++ client/src/Components/Register/Register.jsx
@@ -4,11 +4,30 @@ import {Link} from 'react-router-dom';
 
 function Register() {
     return (
-        <div>
+        <div className="registerContainer">
             <Link to="/">
                 Landing Page
             </Link>
-            <h1>User Registration</h1>
+            {/* <h1>User Registration</h1> */}
+            <div className="registerBox">
+                <form>
+                    <h1 className="formTitle">User Registration</h1>
+                    {/* <label htmlFor="">Username:</label> */}
+                    <input type="text" placeholder="Username"/>
+                    <br />
+                    {/* <label htmlFor="">Email:</label> */}
+                    <input type="email" placeholder="Email"/>
+                    <br />
+                    {/* <label htmlFor="">Password:</label> */}
+                    <input type="password" placeholder="Password"/>
+                    <br />
+                    {/* <label htmlFor="">Re-type password:</label> */}
+                    <input type="password" placeholder="Re-Type Password"/>
+                    <button>Register</button>
+                </form>
+            </div>
+
+
         </div>
     );
 }
